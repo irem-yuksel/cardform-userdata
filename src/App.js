97d1e29ea66c96f0841edf5434e591fc8113fc4a
İrem Yüksel <iremyuksel@Irem-MacBook-Pro.local>
2017-08-07 15:37:31 +0300

    navbar:done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+
 
 class App extends Component {
   render() {
