8f29d08562c8835cba3bb71b43e5ee03a47bd015
İrem Yüksel <iremyuksel@Irem-MacBook-Pro.local>
2017-08-08 15:01:18 +0300

    login works

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import Welcome from './Welcome';
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
 
 
 // var str1 = '[{"formID": "72151581223953", "event_id": "1500279370441_71934126423958_u2wObZp", "resolution": "1680x1050", "totalClickCount": 1, "userAgentString": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36", "totalSessionTime": 2.528}, [{"qid": "id_3", "name": "fullname", "type": "control_fullname", "order": 0, "fields": [{"id": "first_3", "name": "q3_whatIs[first]", "type": "first", "order": 0, "value": -1, "events": [], "length": -1, "fieldfullTime": 0, "characterCounter": 0, "interactionCount": 0}, {"id": "last_3", "name": "q3_whatIs[last]", "type": "last", "order": 1, "value": -1, "events": [], "length": -1, "fieldfullTime": 0, "characterCounter": 0, "interactionCount": 0}], "viewFullTime": 1.681, "questionLength": 23, "totalClickCount": 0, "viewThroughTimes": [1681]}, {"qid": "id_12", "name": "textbox", "type": "control_textbox", "order": 1, "fields": [{"id": "input_12", "name": "q12_whatIs12", "type": "textbox", "order": 0, "value": -1, "events": [], "length": -1, "fieldfullTime": 0, "characterCounter": 0, "interactionCount": 0}], "viewFullTime": 0.847, "questionLength": 23, "totalClickCount": 1, "viewThroughTimes": [0, 847]}, {"qid": "id_1", "name": "email", "type": "control_email", "order": 2, "fields": [{"id": "input_1", "name": "q1_whatIs1", "type": "email", "order": 0, "value": -1, "events": [], "length": -1, "fieldfullTime": 0, "interactionCount": 0}], "viewFullTime": 0, "questionLength": 35, "totalClickCount": 0, "viewThroughTimes": [0]}, {"qid": "id_10", "name": "datetime", "type": "control_datetime", "order": 3, "fields": [{"id": "month_10", "name": "q10_whatIs10[month]", "type": "month", "order": 0, "value": -1, "events": [], "length": -1, "fieldfullTime": 0, "interactionCount": 0}, {"id": "day_10", "name": "q10_whatIs10[day]", "type": "day", "order": 1, "value": -1, "events": [], "length": -1, "fieldfullTime": 0, "interactionCount": 0}, {"id": "year_10", "name": "q10_whatIs10[year]", "type": "year", "order": 2, "value": -1, "events": [], "length": -1, "fieldfullTime": 0, "interactionCount": 0}, {"id": -1, "name": "", "type": "text", "order": 3, "value": "", "events": [], "length": 0, "fieldfullTime": 0, "interactionCount": 0}], "viewFullTime": 0, "questionLength": 27, "totalClickCount": 0, "viewThroughTimes": []}, {"qid": "id_11", "name": "radio", "type": "control_radio", "order": 4, "fields": [{"id": -1, "name": "q11_whatIs11", "type": "radio", "order": 0, "value": "Female", "events": [], "length": 6, "fieldfullTime": 0, "interactionCount": 0}, {"id": -1, "name": "q11_whatIs11", "type": "radio", "order": 1, "value": "Male", "events": [], "length": 4, "fieldfullTime": 0, "interactionCount": 0}], "viewFullTime": 0, "questionLength": 20, "totalClickCount": 0, "viewThroughTimes": []}, {"qid": "id_9", "name": "textarea", "type": "control_textarea", "order": 5, "fields": [], "viewFullTime": 0, "questionLength": 44, "totalClickCount": 0, "viewThroughTimes": []}]]';
@@ -11,40 +7,20 @@ import './App.css';
 // var strarray = [str1, str2, str3];
 
  
-class App extends Component {
-	constructor(props) {
-		super(props);
-
-		this.start = this.start.bind(this);
-		this.onLoginSuccess = this.onLoginSuccess.bind(this);
-		this.onLoginFailure = this.onLoginFailure.bind(this);
-		this.state = {
-			apiKey: null, 
-		};
-	}
+class Welcome extends Component {
 
 	render() {
-		if (this.state.apiKey === null) {
-			return <Welcome start={this.start} />
-		} else {
-			return <FormList apiKey={this.state.apiKey} />
-		}
-	}
-
-	onLoginSuccess() {
-		this.setState({
-			apiKey: global.JF.getAPIKey(),
-		});
-	}
-
-	onLoginFailure() {
-		console.log(arguments);
-	}
-	
-	start(){
-		console.log(this);
-		global.JF.login(this.onLoginSuccess, this.onLoginFailure); 
+		return (
+			<div>
+				<p>Lorem</p>
+				<input
+					type="button"
+					value="Start"
+					onClick={this.props.start}
+				/>
+			</div>
+		);
 	}
 }
 
-export default App;
+export default Welcome;
