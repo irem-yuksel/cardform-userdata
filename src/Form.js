0b54b53da6e9a5cda3e18317077832fa84c33e1d
intern <intern@Cems-MacBook-Air.local>
2017-08-17 16:32:59 +0300

    finally getting the info

--- src/Form.js
+++ src/Form.js
@@ -6,9 +6,12 @@ class Form extends Component {
 		super(props);
 		this.state = {
 			formID : this.props.formID,
-			result : null,
+			result : [],
+			totalSessionTime : 0,
+			noOfSubmissions : 1,
 		};
 		this.renderResult = this.renderResult.bind(this);
+		this.saveResult = this.saveResult.bind(this);
 	}
 	
 	componentWillMount(){
@@ -16,36 +19,45 @@ class Form extends Component {
 		// var url = "https://denis.jotform.pro/server.php?action=getCardformData&formID="+ {this.props.formID};
 
 		fetch(url, {
-			method: 'POST',
-		 	headers: {
-		    'Accept': 'application/jsonp, text/plain, */*',
-		    'Content-Type': 'application/jsonp',
-		    'Access-Control-Allow-Origin': 'http://localhost:3000'
-  			},
-		}).then(res=>res.json())
-  		.then(res=> this.renderResult(res));
+			method: 'GET',
+		 	// headers: {
+		  //   'Accept': 'application/jsonp, text/plain, */*',
+		  //   'Content-Type': 'application/jsonp',
+		  //   'Access-Control-Allow-Origin': 'http://localhost:3000'
+  		// 	},
+		},).then(res=>res.json())
+  		.then(res=> this.saveResult(res));
+	}
+	
+	saveResult(res){
+		console.log(res);
+		this.setState({
+			result : res,
+			noOfSubmissions : res.length,
+		});
+		this.state.result.map(this.renderResult);
+
 	}
 
-
 	renderResult(line) {
-
 		console.log(line);
-		var data = line[0].data;
-		console.log(data);	
-		var iremof=JSON.parse(data["1"]);
-		console.log(data["1"]);	
-		console.log(iremof);	
-		console.log(data["1"].length);	
-		console.log(iremof.length);	
-		 
+		console.log(line.totalSessionTime);
+		var totSessionTime = parseFloat(this.state.totalSessionTime) + parseFloat(line.totalSessionTime);
+		this.setState({
+			totalSessionTime : totSessionTime,
+		});
 
 	}
 
 	render() {
-		// const content = (this.state.result.map(this.renderResult);
+		
 		return (
-			<div>
-								
+			<div>	
+				<h> Form ID: {this.props.formID} </h>
+				<p> This form has <b> {this.state.noOfSubmissions} </b> submissions. </p>
+				<p> <b> Average Total Session Time for this form: </b> { parseFloat(this.state.totalSessionTime)/parseFloat(this.state.noOfSubmissions) } </p>	
+
+
 			</div>		
 		);
 	}
