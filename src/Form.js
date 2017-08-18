450516c207d0f3c287fa472b4b2c9889f60b9d59
intern <intern@Cems-MacBook-Air.local>
2017-08-18 13:49:11 +0300

    shows the avg time spent and left question info now

--- src/Form.js
+++ src/Form.js
@@ -11,11 +11,12 @@ class Form extends Component {
 			noOfSubmissions : 0,
 			noOfQuestions : 0,
 			timeSpentArray : [],
+			leftQuestions : [],
 		};
 		this.renderResult = this.renderResult.bind(this);
 		this.saveResult = this.saveResult.bind(this);
-		this.renderForQuestion = this.renderForQuestion.bind(this);
 		this.calculateTimeSpent = this.calculateTimeSpent.bind(this);
+		this.calculateLeftQuestions = this.calculateLeftQuestions.bind(this);
 	}
 	
 	componentWillMount(){
@@ -24,11 +25,6 @@ class Form extends Component {
 
 		fetch(url, {
 			method: 'GET',
-		 	// headers: {
-		  //   'Accept': 'application/jsonp, text/plain, */*',
-		  //   'Content-Type': 'application/jsonp',
-		  //   'Access-Control-Allow-Origin': 'http://localhost:3000'
-  		// 	},
 		},).then(res=>res.json())
   		.then(res=> this.saveResult(res));
 	}
@@ -40,6 +36,8 @@ class Form extends Component {
 			this.setState({
 				result : res,
 				noOfSubmissions : res.length,
+				timeSpentArray : Array.apply(null, new Array( res[0].data[1].length )).map(Number.prototype.valueOf,0),
+				leftQuestions : Array.apply(null, new Array( res[0].data[1].length )).map(Number.prototype.valueOf,0),
 			});
 			this.state.result.map(this.renderResult);
 		}
@@ -48,38 +46,49 @@ class Form extends Component {
 
 	renderResult(line) {
 		console.log(line);
-		console.log(line.totalSessionTime);
 		var totSessionTime = parseFloat(this.state.totalSessionTime) + parseFloat(line.totalSessionTime);
+		var newTimeSpentArray = Array.apply(null, new Array( line.data[1].length )).map(Number.prototype.valueOf,0);
+		var newLeftQuestions = Array.apply(null, new Array( line.data[1].length )).map(Number.prototype.valueOf,0);
+		console.log(newLeftQuestions);
+
+		for(var i = 0; i < line.data[1].length; i++){
+			newTimeSpentArray[i] = parseFloat(newTimeSpentArray[i]) + parseFloat(line.data[1][i].viewFullTime) + parseFloat(this.state.timeSpentArray[i]);
+			newLeftQuestions[i] = parseFloat(newLeftQuestions[i]) + parseFloat(this.state.leftQuestions[i]);
+
+			if(line.data[1][i].viewFullTime === 0){
+				newLeftQuestions[i] = parseFloat(newLeftQuestions[i]) + parseFloat(1);
+			}	
+		}
+
+		console.log(newTimeSpentArray);
+		console.log(newLeftQuestions);
+
 		this.setState({
 			totalSessionTime : totSessionTime,
 			noOfQuestions : line.data[1].length,
-		});
-		line.data[1].map(this.renderForQuestion);
-	}
-
-	renderForQuestion(question){
-		console.log(question);
-		var newTimeSpentArray = Array.apply(null, new Array( this.state.noOfQuestions )).map(Number.prototype.valueOf,0);
-		if(question.fields.length !== 0){
-			for(var i = 0; i < question.fields[0].events.length; i++){
-				// newTimeSpentArray[] =+  question.fields[0].events[i];
-			}
-		}
-
-		this.setState({
 			timeSpentArray : newTimeSpentArray,
+			leftQuestions : newLeftQuestions,
 		});
 	}
 
-	calculateTimeSpent(item){
-	return (
-	<div>
-		<p> The average time spent for Question #{this.state.timeSpentArray.indexOf(item)}: <b> {item} </b> </p>
-	</div>
-	);
-
+	calculateTimeSpent(item, index){
+		return (
+			<div>
+				<p> The average time spent for Question {index+1}: <b> {item / this.state.noOfSubmissions} </b> </p>
+			</div>
+		);
 	}
 
+
+	calculateLeftQuestions(item, index){
+		if(item !== 0 || !isNaN(item)){
+			return (
+				<div>
+					<p> <b> {item} </b> people left the form before Question #{index+1}. </p>
+				</div>
+			);
+		}
+	}
 
 	render() {
 		const timeContent = this.state.timeSpentArray.map(this.calculateTimeSpent);
@@ -87,6 +96,7 @@ class Form extends Component {
 		if(this.state.noOfSubmissions !== 0){
 			aveSesTime = parseFloat(this.state.totalSessionTime)/parseFloat(this.state.noOfSubmissions);
 		}
+		const leftContent = this.state.leftQuestions.map(this.calculateLeftQuestions);
 
 		return (
 			<div>	
@@ -94,7 +104,7 @@ class Form extends Component {
 				<p> This form has <b> {this.state.noOfSubmissions} </b> submissions. </p>
 				<p> <b> Average Total Session Time for this form: </b> {aveSesTime}	</p>
 				{ timeContent }
-
+				{ leftContent }
 			</div>		
 		);
 	}
